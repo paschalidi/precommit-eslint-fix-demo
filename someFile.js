a5944d80ca88cd4e65c4344a8fe26f05a0733cf4
Samuel Sharpe <samit4me@gmail.com>
2017-04-29 15:51:34 +1000

    Update hook files and add extra file for staged testing

---
+++ someFile.js
@@ -0,0 +1,10 @@
+/**
+ * Add two numbers.
+ * @param {number} num1 The first number.
+ * @returns {number} The sum of the two numbers.
+ */
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+const sum = add(1, 1);
